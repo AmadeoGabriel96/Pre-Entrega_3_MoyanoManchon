fa6ec177af1b4deafb73b725a60ba41f73af8f7c
AmadeoGabriel96 <129233416+AmadeoGabriel96@users.noreply.github.com>
2024-03-10 16:36:22 -0300

    Update models.py

--- AppCoderPE3/models.py
+++ AppCoderPE3/models.py
@@ -2,10 +2,18 @@ from django.db import models
 
 # Create your models here.
 
+from django.db import models
+
+# Create your models here.
+
 class Curso(models.Model): 
 
     nombre = models.CharField(max_length=60)
     camada = models.IntegerField()
+
+    def __str__(self):
+
+        return f"{self.nombre} --- {self.camada}"
 
 class Profesor(models.Model):
 
@@ -15,9 +23,19 @@ class Profesor(models.Model):
     email = models.EmailField()
     profesión = models.CharField(max_length=60)
 
+    def __str__(self):
+
+        return f"{self.nombre} --- {self.apellido}"
+
 class Alumno(models.Model): 
 
     nombre = models.CharField(max_length=60)
     apellido = models.CharField(max_length=60)
     edad = models.IntegerField()
     email = models.EmailField()   
+
+    def __str__(self):
+
+        return f"{self.nombre} --- {self.apellido}"
+
+    
