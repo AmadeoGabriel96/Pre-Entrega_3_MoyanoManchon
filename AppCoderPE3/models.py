04d28b33576e248fb4601ee7997b7e1986290a4d
Amadeo Gabriel <amadeogabrielmm@gmail.com>
2024-03-11 00:44:03 -0300

    versión 1.0 Pre_Entrega_3

--- AppCoderPE3/models.py
+++ AppCoderPE3/models.py
@@ -2,18 +2,10 @@ from django.db import models
 
 # Create your models here.
 
-from django.db import models
-
-# Create your models here.
-
 class Curso(models.Model): 
 
     nombre = models.CharField(max_length=60)
     camada = models.IntegerField()
-
-    def __str__(self):
-
-        return f"{self.nombre} --- {self.camada}"
 
 class Profesor(models.Model):
 
@@ -23,19 +15,9 @@ class Profesor(models.Model):
     email = models.EmailField()
     profesión = models.CharField(max_length=60)
 
-    def __str__(self):
-
-        return f"{self.nombre} --- {self.apellido}"
-
 class Alumno(models.Model): 
 
     nombre = models.CharField(max_length=60)
     apellido = models.CharField(max_length=60)
     edad = models.IntegerField()
     email = models.EmailField()   
-
-    def __str__(self):
-
-        return f"{self.nombre} --- {self.apellido}"
-
-    
